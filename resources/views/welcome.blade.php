f317708f03bdb8727c128b5aa79219db68c52589
Jake Bathman <jake.bathman@gmail.com>
2024-10-07 11:11:50 -0500

    Show headers for request/response

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,35 +11,54 @@
         defer
         src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"
     ></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <title>422 Test</title>
 
 </head>
 
 <body>
-    <div x-data="form">
-        <div>
+    <div
+        x-data="form"
+        class="flex flex-col gap-5"
+    >
+        <div class="text-xl font-bold">
             422 Test
         </div>
+        <div>Click submit with any content, and the server will respond with a 422.</div>
         <div>
             <form @submit.prevent="postForm">
                 @csrf
                 <input
+                    class="border border-gray-300 p-2"
                     type="text"
                     name="email"
                     placeholder="Email"
                 >
-                <button type="submit">Submit</button>
+                <button
+                    class="bg-blue-500 text-white p-2"
+                    type="submit"
+                >Submit</button>
             </form>
         </div>
-        <div>Status: <span x-text="status"></span></div>
-        <div>Response: <span x-text="response"></span></div>
+        <div>
+            <div><span class="font-bold">Status:</span> <span x-text="status"></span></div>
+            <div><span class="font-bold">Response body:</span> <span x-text="response"></span></div>
+            <div><span class="font-bold">Response headers:</span> <span x-text="headers"></span></div>
+        </div>
+
+        <hr />
+        <div class="">
+            <span class="text-lg font-bold">Request headers (for `welcome` request):</span>
+            <pre>@json($headers, JSON_PRETTY_PRINT)</pre>
+        </div>
     </div>
 </body>
 
 <script>
     document.addEventListener('alpine:init', () => {
         Alpine.data('form', () => ({
+            headers: null,
             response: null,
             status: null,
 
@@ -56,6 +75,15 @@
                         })
                     })
                     .then(response => {
+                        console.debug(response)
+                        let headers = {}
+
+                        response.headers.forEach((value, key) => {
+                            headers[key] = value;
+                        });
+
+                        this.headers = JSON.stringify(headers)
+
                         this.status = response.status
                         return response.json()
                     })
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Test422Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'headers' => request()->headers->all(),
+    ]);
 });
 
 Route::any('/test', Test422Controller::class)->name('test');
